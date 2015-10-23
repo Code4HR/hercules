48b3c811b5424bb94eb55bf9cc2b27b3786bf157
Derek Drummond <Derek.Drummond@DominionEnterprises.com>
2015-10-23 14:17:34 -0400

    Added backend Framework.

---
+++ tests/HDS/CrimeConsumerTests.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Defines CrimeConsumer unit tests.
+ */
+
+
+/**
+ * Unit Tests for CrimConsumer
+ */
+ public final class CrimeConsumerTests{
+
+     public function insertVABeachRecord()
+     {
+
+     }
+
+     public function insertNorfolkRecord()
+     {
+
+     }
+
+     public function insertNpnRecord()
+     {
+
+     }
+
+     public function insertChesapeake()
+     {
+
+     }
+
+     public function insertSuffolkRecord()
+     {
+
+     }
+
+     public function insertPortsmouthRecord()
+     {
+
+     }
+ }
