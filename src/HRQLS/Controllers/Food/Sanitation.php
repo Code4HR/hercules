eb69a8651b40903cb9d458c5d89f5d5761c04b63
krishna <krishna.murthy@dominionenterprises.com>
2015-10-24 10:39:10 -0400

    updated code

--- src/HRQLS/Controllers/Food/Sanitation.php
+++ src/HRQLS/Controllers/Food/Sanitation.php
@@ -9,25 +9,38 @@ use Symfony\Component\HttpFoundation\Response as Response;
 class Sanitation
 {
     public $cities = [
-        'norfolk' => 'http://api.openhealthinspection.com/vendors?city=norfolk',
-        'portsmouth' => 'http://api.openhealthinspection.com/vendors?city=portsmouth',
-        'virginiabeach' => 'http://api.openhealthinspection.com/vendors?city=virginia%20beach'
+        'norfolk' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=norfolk',
+        'portsmouth' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=portsmouth',
+        'virginiabeach' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=virginia%20beach',
+        'suffolk' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=suffolk',
+        'newportnews' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=newport%20news'
     ];
 
     public function main(Request $req, Application $app)
     {
         $sliderPercentage = $req->get('slidervalue');
+
         foreach($this->cities as $city => $url) {
             $result = file_get_contents($url);
             $response = json_decode($result, true);
         }
         
         $sanitationdata = [];
-        foreach($response as $key => $res) {
-            $data['latitude'] = $res['coordinates']['latitude'];
-            $data['longitude'] = $res['coordinates']['longitude'];
-            $data['score'] = $res['score'];
-            $sanitationdata[] = $data;
+        foreach ([50, 75, 80, 90] as $value) {
+            $updatedslidervalue = (($sliderPercentage * (100 - $value) ) / 100) + $value;
+            $sanitationdata = [];
+            foreach($response as $key => $res) {
+                $data['name'] = $res['name'];
+                $data['latitude'] = $res['coordinates']['latitude'];
+                $data['longitude'] = $res['coordinates']['longitude'];
+                $score = $res['score'];
+                if ($score < $updatedslidervalue) {
+                    $sanitationdata[] = $data;
+                }
+            }
+            if (count($sanitationdata)) {
+                break;
+            }
         }
 
         return new Response(json_encode($sanitationdata), 201);
