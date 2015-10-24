d6dabbc97139d674c552be6354edc6895e4f305a
alicia.sedarski <alicia.sedarski@dominionenterprises.com>
2015-10-24 11:35:49 -0400

    3 data sources

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,6 @@
 function initAutocomplete() {
 
-    var map, heatmap, foodSanitationMarker;
+    var map, crimeheatmap, colheatmap, foodSanitationMarker;
     var heatMapData = [];
     var Datas = [];
     var foodSanitationImage = "../imgs/foodSanitationImage.png"
@@ -36,19 +36,26 @@ function initAutocomplete() {
         map = new google.maps.Map(document.getElementById('map'),
                                   mapOptions);
         var pointArray = new google.maps.MVCArray(Datas);
-        heatmap = new google.maps.visualization.HeatmapLayer({
+        crimeheatmap = new google.maps.visualization.HeatmapLayer({
             data: pointArray,
             maxIntensity: 12,
             dissipate: false
         });
-        heatmap.setMap(map);
+        crimeheatmap.setMap(map);
+        colheatmap = new google.maps.visualization.HeatmapLayer({
+            data: pointArray,
+            maxIntensity: 12,
+            dissipate: false,
+            radius: 100
+        });
+        colheatmap.setMap(map);
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 
     $('#crimeSlider').change(function(){
-        var newHeatMapData = [];
+        var crimeHeatMapData = [];
         var data = {};
-        data.sliderValue = $(this).text();
+        data.slidervalue = $(this).text();
         $.ajax({
             method: 'GET',
             url: '/crime',
@@ -56,16 +63,34 @@ function initAutocomplete() {
             dataType: 'json',
             success: function(crimeData) {
                 for (var elem = 0, max = crimeData.length; elem < max; elem++) {
-                    newHeatMapData.push({location: new google.maps.LatLng(crimeData[elem].latitude, crimeData[elem].longitude), weight: crimeData[elem].severity});
+                    crimeHeatMapData.push({location: new google.maps.LatLng(crimeData[elem].latitude, crimeData[elem].longitude), weight: crimeData[elem].severity});
                 }
-                heatmap.set('data', newHeatMapData);
+                crimeheatmap.set('data', crimeHeatMapData);
+            }
+        });
+    });
+    $('#colSlider').change(function(){
+        var colHeatMapData = [];
+        var data = {};
+        data.slidervalue = $(this).text();
+        $.ajax({
+            method: 'GET',
+            url: '/col',
+            data: data,
+            dataType: 'json',
+            success: function(colData) {
+                for (var elem = 0, max = colData.length; elem < max; elem++) {
+                    colHeatMapData.push({location: new google.maps.LatLng(colData[elem].lat, colData[elem].lon), weight: colData[elem].weight});
+                }
+                colheatmap.set('data', colHeatMapData);
             }
         });
     });
     $('#foodSanitationSlider').change(function(){
         var newHeatMapData = [];
         var data = {};
-        data.sliderValue = $(this).text();
+        data.slidervalue = $(this).text();
+        alert(data.slidervalue);
         $.ajax({
             method: 'GET',
             url: '/food/sanitation',
@@ -76,6 +101,7 @@ function initAutocomplete() {
                     if (foodSanitationData[elem].latitude !== null && foodSanitationData[elem].longitude !== null){
                         foodSanitationMarker = new google.maps.Marker({
                             position: {lat: foodSanitationData[elem].latitude, lng: foodSanitationData[elem].longitude},
+                            title: foodSanitationData[elem].name,
                             map: map,
                             icon: foodSanitationImage
                         })
@@ -84,6 +110,8 @@ function initAutocomplete() {
             }
         });
     });
+
+
 
     //    // Create the search box and link it to the UI element.
     //    var input = document.getElementById('pac-input');
