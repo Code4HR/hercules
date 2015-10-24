476c8f41e4cc8e5446be603df3efc5d315b31a00
krishna <krishna.murthy@dominionenterprises.com>
2015-10-23 20:07:53 -0400

    added crime

--- src/HRQLS/Controllers/Crime.php
+++ src/HRQLS/Controllers/Crime.php
@@ -5,13 +5,29 @@ namespace HRQLS\Controllers;
 use Silex\Application as Application;
 use Symfony\Component\HttpFoundation\Request as Request;
 use Symfony\Component\HttpFoundation\Response as Response;
+use Elasticsearch\Client;
 
 class Crime
 {
     public function main(Request $req, Application $app)
     {
-        return $app['twig']->render('mainPage.twig', array(
-            'title' => 'Hercules'
-        ));
+       $client = new Client(['hosts' => ['http://localhost:9200']]);
+       $sliderPercentage = $req->get('slidervalue') / 100; 
+       $slider = floor($sliderPercentage * 12) ;
+       $params = array(
+            'index'  => 'hrqls',
+            'type'   => 'crimedata',
+            'body' => [
+                'query' => [
+                    'range' => [
+                        'severity' => [
+                            'gte' => $slider
+                        ]
+                    ]
+                ]
+            ] 
+       );
+       $result = $client->search($params)['hits'];
+       return new Response(json_encode($result), 201);
     } 
 }
