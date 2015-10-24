ba2794aa27527ed9134c6d4f708e2722b8ce75df
krishna <krishna.murthy@dominionenterprises.com>
2015-10-24 11:08:19 -0400

    updated code

--- src/HRQLS/Controllers/Food/Sanitation.php
+++ src/HRQLS/Controllers/Food/Sanitation.php
@@ -9,33 +9,36 @@ use Symfony\Component\HttpFoundation\Response as Response;
 class Sanitation
 {
     public $cities = [
-        'norfolk' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=norfolk',
-        'portsmouth' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=portsmouth',
-        'virginiabeach' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=virginia%20beach',
-        'suffolk' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=suffolk',
-        'newportnews' => 'http://api.openhealthinspection.com/inspections?after=24-10-2014&city=newport%20news'
+        'norfolk' => 'http://api.openhealthinspection.com/vendors?after=24-10-2014&city=norfolk',
+        'portsmouth' => 'http://api.openhealthinspection.com/vendors?after=24-10-2014&city=portsmouth',
+        'virginiabeach' => 'http://api.openhealthinspection.com/vendors?after=24-10-2014&city=virginia%20beach',
+        'suffolk' => 'http://api.openhealthinspection.com/vendors?after=24-10-2014&city=suffolk',
+        'newportnews' => 'http://api.openhealthinspection.com/vendors?after=24-10-2014&city=newport%20news'
     ];
 
     public function main(Request $req, Application $app)
     {
         $sliderPercentage = $req->get('slidervalue');
-
+        $response = [];
         foreach($this->cities as $city => $url) {
             $result = file_get_contents($url);
-            $response = json_decode($result, true);
+            array_push($response, json_decode($result, true));
         }
-        
+
         $sanitationdata = [];
         foreach ([50, 75, 80, 90] as $value) {
             $updatedslidervalue = (($sliderPercentage * (100 - $value) ) / 100) + $value;
             $sanitationdata = [];
-            foreach($response as $key => $res) {
-                $data['name'] = $res['name'];
-                $data['latitude'] = $res['coordinates']['latitude'];
-                $data['longitude'] = $res['coordinates']['longitude'];
-                $score = $res['score'];
-                if ($score < $updatedslidervalue) {
-                    $sanitationdata[] = $data;
+            foreach($response as $key => $res) { 
+                foreach ($res as $toldata) {
+                    $data['name'] = $toldata['name'];
+                    $data['latitude'] = $toldata['coordinates']['latitude'];
+                    $data['longitude'] = $toldata['coordinates']['longitude'];
+                    $data['city'] = $toldata['city'];
+                    $score = $toldata['score'];
+                    if ($score >= $updatedslidervalue) {
+                        $sanitationdata[] = $data;
+                    }
                 }
             }
             if (count($sanitationdata)) {
