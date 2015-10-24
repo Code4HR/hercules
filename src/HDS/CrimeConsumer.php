7937b18c9f38cab1d0078dd5c7143f2d69c68e92
Derek Drummond <Derek.Drummond@DominionEnterprises.com>
2015-10-24 11:08:09 -0400

    Refactored Crime Scraper.

--- src/HDS/CrimeConsumer.php
+++ src/HDS/CrimeConsumer.php
@@ -4,7 +4,13 @@ include __DIR__ . '/../../vendor/autoload.php';
 use elasticsearch\Client;
 use Utils\Scraper;
 
-$consumer = new CrimeConsumer('VA Beach', 'http://hamptonroads.com/newsdata/crime/virginia-beach/search/rss?me=/virginia-beach/search&type=&near=&radius=&op=Submit&form_token=9dc84572393ad9c68f54cad6549692f3&form_id=crime_searchform');
+$consumer = new CrimeConsumer('VA Beach', 'http://hamptonroads.com/newsdata/crime/virginia-beach/search/rss?type=&near=&radius=&from%5Bmonth%5D=10&from%5Bday%5D=1&from%5Byear%5D=2015&to%5Bmonth%5D=10&to%5Bday%5D=23&to%5Byear%5D=2015&op=Submit&form_id=crime_searchform');
+$consumer->consume();
+$consumer = new CrimeConsumer('Norfolk', 'http://hamptonroads.com/newsdata/crime/norfolk/search/rss?me=%2Fnorfolk%2Fsearch&type=&near=&radius=&op=Submit&form_token=9dc84572393ad9c68f54cad6549692f3&form_id=crime_searchform');
+$consumer->consume();
+$consumer = new CrimeConsumer('Portsmouth', 'http://hamptonroads.com/newsdata/crime/portsmouth/search/rss?type=&near=&radius=&from%5Bmonth%5D=10&from%5Bday%5D=1&from%5Byear%5D=2015&to%5Bmonth%5D=10&to%5Bday%5D=23&to%5Byear%5D=2015&op=Submit&form_id=crime_searchform');
+$consumer->consume();
+$consumer = new CrimeConsumer('Suffolk', 'http://hamptonroads.com/newsdata/crime/suffolk/search/rss?type=&near=&radius=&from%5Bmonth%5D=10&from%5Bday%5D=1&from%5Byear%5D=2015&to%5Bmonth%5D=10&to%5Bday%5D=23&to%5Byear%5D=2015&op=Submit&form_id=crime_searchform');
 $consumer->consume();
 
 class CrimeConsumer{
@@ -26,7 +32,7 @@ class CrimeConsumer{
         $currPage = 0;
         $prevPage = 0;
         do {
-           $json = $this->scraper->scrapeCrime($currPage, $this->city);
+           $json += $this->scraper->scrapeCrime($currPage, $this->city);
            if (sizeof($json) > 0) {
                $this->insertIntoElasticSearch($json);
            }
@@ -34,6 +40,10 @@ class CrimeConsumer{
            $currPage++;
         }
         while (count($json)%35 === 0);
+
+        foreach($json as $crime) {
+            echo $crime . PHP_EOL;
+        }
     }
 
     private function insertIntoElasticSearch($json)
