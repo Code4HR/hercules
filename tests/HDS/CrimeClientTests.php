6204f042ec9ed013507777ed6d4ce90306331096
Derek Drummond <Derek.Drummond@DominionEnterprises.com>
2015-12-06 00:33:49 -0500

    Renamed CrimeConsumer to CrimeClient for consistent naming structure. Also merged Scraper into CrimeClient.

---
+++ tests/HDS/CrimeClientTests.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Defines CrimeConsumer unit tests.
+ */
+
+
+/**
+ * Unit Tests for CrimConsumer
+ */
+final class CrimeConsumerTests{
+
+     public function consume_BasicUse()
+     {
+     }
+ }
