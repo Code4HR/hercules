353da9a13aa0a3d7a4e9ad6457a480b7645ad197
Jason Bennett <jason.bennett@dominionenterprises.com>
2016-01-14 17:36:37 -0500

    Minor fixes
    - Corrected possible fail point in build of docker container
    - Corrected version of guzzle
    - Started normalizing elasticsearch connections in code

---
+++ tests/HRQLS/Controllers/CrimeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use HRQLS\Controllers\Crime;
+
+class CrimeTest extends PHPUnit_Framework_TestCase
+{
+    public function testCrimeController()
+    {
+        $crime = new Crime();
+
+
+    }
+}
