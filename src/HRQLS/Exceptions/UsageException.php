5bcfe0b92195a87e0d54dfb6e09bfd41826f0c13
Derek Drummond <derek.ac.drummond@gmail.com>
2016-05-07 08:01:05 -0700

    Added Custom UsageException.

---
+++ src/HRQLS/Exceptions/UsageException.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Defines a custom exception thrown by the HRQLS framework.
+ *
+ * @package HRQLS\Exceptions
+ */
+ 
+ namespace HRQLS\Exceptions;
+ 
+ /**
+  * Defines thecustom USageException class.
+  */
+class UsageException extends Exception
+{
+    /**
+     * Constructs a new UsageException object.
+     *
+     * @param string    $message  The message describinng why the exception occured.
+     * @param integer   $code     The error code for this specific error. Defaults to 0.
+     * @param Exception $previous The previous exception that caused this exception.
+     */
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+     
+    /**
+     * Canonocalizes UsageException for easier human readable output.
+     *
+     * @return string The error code and error message concatenated into a string.
+     */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
