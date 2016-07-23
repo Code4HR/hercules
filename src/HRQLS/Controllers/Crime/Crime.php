69e65eee6aa01098cd062dbc5829970e31d28451
Derek Drummond <derek.ac.drummond@gmail.com>
2016-07-23 13:02:52 -0700

    Added some necessary info to base Crime endpoint.

--- src/HRQLS/Controllers/Crime/Crime.php
+++ src/HRQLS/Controllers/Crime/Crime.php
@@ -15,10 +15,13 @@ final class Crime
     /**
      * This is the main entry point for Crime endpoints.
      *
+     * @param Request     $req The request to be handled.
+     * @param Application $app The silex application that will handle the request.
+     *
      * @return array A list of all the Crime endpoints under /crime.
      */
-    public function main()
+    public function main(Request $req, Application $app)
     {
-        return 'This endpoint is really set up yet. Check back later...but not to soon.';
+        return ['This endpoint is really set up yet. Check back later...but not to soon.'];
     }
 }
