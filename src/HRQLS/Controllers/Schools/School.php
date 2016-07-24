7ef571776d1a4a27fd02582fd9ae3f3927b67547
Derek Drummond <derek.ac.drummond@gmail.com>
2016-07-24 18:12:51 -0400

    Added support to query all supported cities by hitting /schools endpoint. This also has the ?cities query string parameter which will let you query for any combination of the cities.

--- src/HRQLS/Controllers/Schools/School.php
+++ src/HRQLS/Controllers/Schools/School.php
@@ -8,6 +8,7 @@
 namespace HRQLS\Controllers\Schools;
 
 use Silex\Application;
+use HRQLS\Models\HerculesResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -29,19 +30,28 @@ final class School
      * @param Request     $req The request to be handled.
      * @param Application $app The Silex Application used to handle the request.
      *
-     * @return array An array of school datapoints for Hampton, VABeach, Norfolk, Portsmouth, Chesapeake, and Suffolk.
+     * @return array An array of datapoints describing schools in Hampton, VABeach, Norfolk, Portsmouth, Chesapeake, and Suffolk.
      */
     public function main(Request $req, Application $app)
     {
-        return json_encode([
-            'get' => [
-                '/hampton',
-                '/norfolk',
-                '/vabeach',
-                '/suffolk',
-                '/portsmouth',
-                '/chesapeake',
-            ],
-        ]);
+        $requestedCities = ['Hampton', 'Norfolk', 'Virginia Beach', 'Portsmouth', 'Chesapeake', 'Suffolk'];
+        
+        if ($req->query->has('cities')) {
+            $requestedCities = explode(',', $req->query->get('cities'));
+        }
+        
+        $resultSet = [];
+        foreach ($requestedCities as $requestedCity) {
+            $url = SchoolUtils::formatRequestUrl($requestedCity);
+            $response = $app['guzzle']->get($url, []);
+            
+            $schools = SchoolUtils::convertToJson($response->getBody());
+            $resultSet[] = SchoolUtils::filterResultsByCity($schools, $requestedCity);
+        }
+        
+        $herculesResponse = new HerculesResponse('/schools', 200, $resultSet);
+        
+        // The frontend expects a JSONP format, to do this the response must be wrapped in a callback.
+        return $_GET['callback'] . '(' . $herculesResponse->to_json() . ')';
     }
 }
